19f8c5acf1f2f3f6a0be68ca868d1e70c07313c9
Aidan Nuzum-Clark <aidan@Aidans-Laptop.local>
2024-12-24 01:41:58 -0500

    add: small validation to server

--- backend/api_server.ts
+++ backend/api_server.ts
@@ -3,17 +3,26 @@ import HostGame from "./wss";
 const app = express();
 const port = 4000;
 let basePort = 8000;
+
+const maxGames = 3;
+let numGames = 0;
+
 app.get("/createGame", (req, res) => {
 	const playerCount = Number(req.query.numPlayers);
 	console.log(`Creating game with ${playerCount} players`);
-	res.send({message: "Game created", port: ++basePort});
-	HostGame(basePort, playerCount);
-});
 
-// app.post('/api/data', (req, res) => {
-// 	const data = req.body;
-// 	res.json({ receivedData: data });
-//   });
+	if (playerCount < 2 || playerCount > 4) {
+		res.status(400).send({ message: "Invalid number of players" });
+		return;
+	} else if (numGames >= maxGames) {
+		res.status(400).send({ message: "Maximum number of games reached" });
+		return;
+	} else {
+		numGames++;
+		res.send({ message: "Game created", port: ++basePort });
+		HostGame(basePort, playerCount);
+	}
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
