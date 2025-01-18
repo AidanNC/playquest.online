19f099ad5523207cbc0dc876d32ac4dd888dc2b0
Aidan Nuzum-Clark <aidan@Aidans-Laptop.local>
2025-01-18 18:23:16 -0500

    add: patch notes, fix: modals now are centered on larger screens

---
+++ frontend/playquest.online-frontend/src/components/PatchNotes.tsx
@@ -0,0 +1,53 @@
+import ModalContainer from "./ModalContainer";
+import styled from "styled-components";
+import { useState } from "react";
+
+const NotificationContainer = styled.div`
+	position: absolute;
+	top: 20px;
+	right: 5px;
+	height: 50px;
+	width: 50px;
+	border-radius: 100%;
+	background-color: var(--main-pink);
+	z-index: 100;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 1.5rem;
+	text-align: center;
+	border: 1px solid var(--main-yellow);
+	
+	&:hover{
+		cursor: pointer;
+		box-shadow: 0px 0px 10px 0px var(--main-yellow);}
+`;
+const notes = [
+	"- Added patch notes!",
+	"- Fixed bug displaying incorrect login status.",
+	"- Fixed bug allowing too many users to join a game causing it to crash.",
+	"- Added new profile picture options.",
+	"- Make modals centered on all devices."
+]
+const patchNumber = 0.1;
+export default function PatchNotes() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		isOpen ? (
+			<ModalContainer onClose={() => setIsOpen(false)} isOpen={isOpen}>
+				<h1>Patch Notes v{patchNumber}</h1>
+				<p>{notes.map((note)=>{
+					return <li>{note}</li>
+				})}</p>
+			</ModalContainer>
+		) 
+		: 
+		(
+			<NotificationContainer onClick={() => setIsOpen(true)}>
+				📝
+			</NotificationContainer>
+			
+		)
+	)
+}
